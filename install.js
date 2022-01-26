19cdf89a639924fa279cb77785c76d97dd257dd8
NRD <85891552+NRDsstuff@users.noreply.github.com>
2022-01-26 11:33:30 +0100

    b

--- install.js
+++ install.js
@@ -2,7 +2,7 @@ module.exports = (rl) =>{
 
     const NZTK = require('../../../other/NZTK')
 
-    NZTK.moveFile('./SHELL/temp/NZPM/ISH-history-main/programs/history.js', './SHELL/programs/history.js')
+    NZTK.moveFile('./SHELL/temp/NZPM/ISH-history-main/programs/history.js', './SHELL/programs/history.js', "there was an error while installing the package history", true)
     NZTK.removedir('./SHELL/temp/NZPM/ISH-history')
     return NZTK.log("finished installing history", "NZPM", "installing")
 }
