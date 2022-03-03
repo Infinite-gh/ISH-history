2f3053d484e3dd25e88a6dc3ce1801cfdd21c425
NRD <85891552+NRDsstuff@users.noreply.github.com>
2022-03-03 16:29:19 +0100

    new install format

--- install.js
+++ install.js
@@ -2,12 +2,11 @@ module.exports = (rl) =>{
 
     const NZTK = require('../../../other/NZTK')
     const fs = require('fs')
-
+    let installed = require('../../../configs/NZPM/toupdate.json')
+    
     NZTK.moveFile('./SHELL/temp/NZPM/history/programs/history.js', './SHELL/programs/history.js', "there was an error while installing the package history", true)
     NZTK.removedir('./SHELL/temp/NZPM/history')
-    fs.appendFile('./SHELL/configs/NZPM/installed.txt', "history", "history", (err) =>{
+    installed.packages.push("history")
 
-        console.log(" ")
-    })
     return NZTK.log("finished installing history", "NZPM", "installing")
 }
