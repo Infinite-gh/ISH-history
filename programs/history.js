92d1386c8c53b0e16fcedf009f761e36bcd13f90
NRD <85891552+NRDsstuff@users.noreply.github.com>
2022-05-03 21:06:14 +0200

    Update history.js

--- programs/history.js
+++ programs/history.js
@@ -7,7 +7,7 @@ module.exports = {
         
         const fs = require('fs')
 
-        fs.readFile('./SHELL/logs/NZSHH/history.txt', 'utf8', (err, data) =>{
+        fs.readFile('./SHELL/logs/NZSHH/cmdhandler.txt', 'utf8', (err, data) =>{
             if(err){
                 console.log(`an error occured while reading the history of minus shell`)
             }else{
